3d964bdb9c690c2b2df8d0d2af16cbbe6df2dfc6
Draike13 <tmbiri91@gmail.com>
2025-01-24 15:51:08 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// basicrandom number function
+function randomNum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function sendData() {
+  //collect data from form
+}
+//test function
+function test() {
+  console.log('just testing');
+}
