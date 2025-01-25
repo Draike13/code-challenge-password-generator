5158c5de3af990eff502f81e54764fa2b35e4fe5
Draike13 <tmbiri91@gmail.com>
2025-01-24 18:49:41 -0600

    write random letter function

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
+let data = [];
+
 // basic random number function
 function randomNum(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//specific password letter function
+function lettersOnly() {
+  let holding = [];
+  for (let i = 0; i < data[0]; i++) {
+    console.log(password.letters[Math.floor(Math.random() * 26)]);
+  }
+  //   console.log(holding);
 }
 
 //functions for generating password at random
@@ -15,7 +26,27 @@ let password = {
 
 function sendData(length, upLow, num, special) {
   //collect data from form
-  console.log(`${length},${upLow},${num},${special}.`);
+  data[0] = Number(length);
+  if (upLow === 'upper') {
+    data[1] = 0;
+  } else if (upLow === 'lower') {
+    data[1] = 1;
+  } else return console.error('nothing selected');
+
+  if (num === 'number') {
+    data[2] = 0;
+  } else if (num === 'no-number') {
+    data[2] = 1;
+  } else return console.error('nothing selected');
+
+  if (special === 'special') {
+    data[3] = 0;
+  } else if (special === 'basic') {
+    data[3] = 1;
+  } else return console.error('nothing selected');
+
+  console.log(data);
+  //   console.log(`${length},${upLow},${num},${special}.`);
 }
 //test function
 function test() {
