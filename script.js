17378e94e85b927d808910e432bea50b4e9a79b1
Draike13 <tmbiri91@gmail.com>
2025-01-24 18:01:35 -0600

    create form submission

--- script.js
+++ script.js
@@ -3,14 +3,25 @@ function randomNum(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function sendData() {
+//functions for generating password at random
+let password = {
+  letters: 'abcdefghijklmnopqrstuvwxyz',
+  capital: function () {
+    return this.letters.toUpperCase();
+  },
+  symbols: '!@#$%^&*()-',
+  numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+};
+
+function sendData(length, upLow, num, special) {
   //collect data from form
+  console.log(`${length},${upLow},${num},${special}.`);
 }
 //test function
 function test() {
   console.log('just testing');
 }
-
+//event listener to run something after the dom has loaded once. solves for 'null' response error
 document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('password-box').defaultValue = 'Password';
 });
